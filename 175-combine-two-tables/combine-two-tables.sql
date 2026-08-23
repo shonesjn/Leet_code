4eaa0c5e89077898a2da84e2be87058aa4b89c87
Shone Sajan <shonesajanstjohns@gmail.com>
2026-08-23 19:55:25 +0530

    Time: 685 ms (6.33%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,8 +1,9 @@
 SELECT 
-    p.firstName,
-    p.lastName,
-    a.city,
-    a.state
-FROM Person p
-LEFT JOIN Address a
-    ON p.personId = a.personId;
+    Person.firstName, 
+    Person.lastName, 
+    Address.city, 
+    Address.state
+FROM 
+    Person
+LEFT JOIN 
+    Address ON Person.personId = Address.personId;
