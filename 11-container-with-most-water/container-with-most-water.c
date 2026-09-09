15fef5b8fd4e41496a2ecdb9fa8c7e4879ac17bf
Shone Sajan <shonesajanstjohns@gmail.com>
2026-09-09 15:29:41 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (10.63%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.c
+++ 11-container-with-most-water/container-with-most-water.c
@@ -1,30 +1,31 @@
-#include <stdio.h>
-
 int maxArea(int* height, int heightSize) {
+
     int left = 0;
     int right = heightSize - 1;
-    int max_water = 0;
+    int maxWater = 0;
 
     while (left < right) {
-        // Calculate the width between pointers
-        int width = right - left;
-        
-        // Find the limiting height (shorter of the two lines)
-        int current_height = height[left] < height[right] ? height[left] : height[right];
-        
-        // Calculate current area and update max_water if it's larger
-        int current_area = width * current_height;
-        if (current_area > max_water) {
-            max_water = current_area;
-        }
 
-        // Move the pointer corresponding to the shorter line inward
-        if (height[left] < height[right]) {
+        int width = right - left;
+
+        int minHeight;
+
+        if (height[left] < height[right])
+            minHeight = height[left];
+        else
+            minHeight = height[right];
+
+        int water = width * minHeight;
+
+        if (water > maxWater)
+            maxWater = water;
+
+        // Move the shorter line
+        if (height[left] < height[right])
             left++;
-        } else {
+        else
             right--;
-        }
     }
 
-    return max_water;
+    return maxWater;
 }
